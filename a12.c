613505a07be3174288d0bb279a66c6520d474f70
Pradeep Surabhi <pradeep.surabhi@intel.com>
2024-02-09 10:01:11 +0530

    feat: Updated a12.c

--- a12.c
+++ a12.c
@@ -18,3 +18,21 @@ int main() {
     fibonacci(n);
     return 0;
 }
+int isFibonacci(int n) {
+    if (n <= 0)
+        return 0;
+    int t1 = 0, t2 = 1, nextTerm = t1 + t2;
+    while (nextTerm < n) {
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+    return nextTerm == n;
+}
+        printf("%d is ", testNumbers[i]);
+        if (!isFibonacci(testNumbers[i]))
+            printf("not ");
+        printf("a Fibonacci number.\n");
+    }
+    return 0;
+}
