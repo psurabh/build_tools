b300381a7ef3f8b767964386c1b7c025abc2568b
Pradeep Surabhi <pradeep.surabhi@intel.com>
2024-02-27 10:44:58 +0530

    feat: Implement binary search algorithm in Python

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+"""
+This file implements a binary search algorithm.
+"""
+
+def binary_search(lst, key):
+    if not lst:
+        return -1
+    if lst != sorted(lst):
+        return -1
+
+    low, high = 0, len(lst) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == key:
+            return mid
+        elif lst[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    print(binary_search([1, 2, 3, 4, 5], 3))  # Expected output: 2
+    print(binary_search([1, 2, 3, 4, 5], 6))  # Expected output: -1
+    print(binary_search([], 1))  # Expected output: -1
+    print(binary_search([2, 1], 1))  # Expected output: -1
